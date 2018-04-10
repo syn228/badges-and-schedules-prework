0cb7aeacc69dc327270a848e1e97637221cd484f
Aaron Na <syn228@nyu.edu>
2018-04-10 21:54:04 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,14 @@
-# Write your code here.
+def badge_maker(name)
+   "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  counter = 0
+  while counter < attendees.length
+  return  "#{badge_maker(attendees[counter]}."
+  counter += 1
+  
+  
+  end
+end
+
